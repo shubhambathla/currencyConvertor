8cf3cd252a250502c333ddc34dfbd564e8686c11
shubhambathla <shubhambathla116@gmail.com>
2024-04-03 02:24:53 -0400

    UI, display of JSON data and new API updated

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,49 +1,48 @@
 import React, {useId} from 'react'
 
-
 function InputBox({
     label,
     amount,
-    onAmountChange, //
+    onAmountChange,
     onCurrencyChange,
-    currencyOptions = ["inr", "usd"], // empty array to keep safe from app crash
-    selectCurrency = "usd", //default currency
-    amountDisable = false, //user are allowed to change amount, not usefull but good for production
-    currencyDisable = false, //user are allowed to change currency, not usefull but good for production
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
     className = "",
 }) {
-   
-    const amountInputId = useId() // used only for study purpose for binding label and Input, we could have also passed any random value instead of "Id"
+   const amountInputId = useId()
+
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label htmlFor = 'amountInputId' className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    id = {amountInputId} 
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
-                    disabled = {amountDisable}
-                    value = {amount}
-                    onChange = {(e)=> onAmountChange && onAmountChange(Number(e.target.value))} // checker to check if onAmountChange exist or not, used Number to be safe if String is returned in value
-                /> 
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
-                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none" 
-                    value = {selectCurrency}
-                    onChange = {(e) => onCurrencyChange && onCurrencyChange(e.target.value)} // checker to check if onCurrencyChange exist or not,
-                    disabled = {currencyDisable}
+                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
                 >
-                    {currencyOptions.map((currency) => (
-                        <option key = {currency} value= {currency}> 
-                             {currency}
-                         </option> //used key to improve performance
-                    ))}
-                   
+                    
+                        {currencyOptions.map((currency) => (
+                            <option key={currency} value={currency}>
+                            {currency}
+                            </option>
+                        ))}
                 
                 </select>
             </div>
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
   const [data, setData] = useState({}); //{} empty object for making it safe(contengcy plan) from crash in case of empty response, so that we can loop on empty object
@@ -6,10 +6,11 @@ function useCurrencyInfo(currency) {
     fetch(
       `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
     )
-      .then((res) => res.json) // response converted to json format
+      .then((res) => res.json()) // response converted to json format
       .then((res) => setData(res[currency])); // assign json data to useState so that it can be updated in UI, also to access to using key value usin res[currency]
     console.log(data);
-  }, [currency]); // whenever there will be nay change in currency, useEffect will be fired again
+  }, [currency]); // whenever there will be any change in currency, useEffect will be fired again
+  console.log(data);
   return data;
 }
 
